1511a82ef475a2b8c8c78588e36ff3461a916cc6
jun-low <zhijun.low@gmail.com>
2020-11-07 22:01:56 +0800

    add 2.6: The Phonebook Step1

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,18 +6,28 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const formSubmit = (e) => {
+    console.log(e)
+    e.preventDefault();
+    setPersons(persons.concat({ name: newName }));
+    setNewName('');
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={formSubmit}>
         <div>
-          name: <input />
+          name:<input value={newName} onChange={(e) => setNewName(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   )
 }
